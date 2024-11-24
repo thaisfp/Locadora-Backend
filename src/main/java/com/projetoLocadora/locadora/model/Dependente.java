6c64ff0093cd01d3c259c5db2dc09832df556d06
Thais Ferreira Passos <thispassos55@gmail.com>
2024-11-24 01:40:30 -0300

    feat: modificando model de Socio

--- src/main/java/com/projetoLocadora/locadora/model/Dependente.java
+++ src/main/java/com/projetoLocadora/locadora/model/Dependente.java
@@ -1,13 +1,10 @@
 package com.projetoLocadora.locadora.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import lombok.Data;
 
 @Data
 @Entity
 public class Dependente extends Cliente{
     
-    @JoinColumn(name = "idCliente")
-    private Socio socio;
 }
--- src/main/java/com/projetoLocadora/locadora/model/Socio.java
+++ src/main/java/com/projetoLocadora/locadora/model/Socio.java
@@ -1,15 +1,24 @@
 package com.projetoLocadora.locadora.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
 @Entity
 public class Socio extends Cliente{
     
+    
     private String cpf;
 
     private String endereco;
 
     private String tel;
+
+    @OneToMany
+    @JoinColumn(name = "dependente")
+    private List<Dependente> dependentes;
 }
